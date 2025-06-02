146f2f821e339c71abbc7134fbfc337f59216994
smvvalli <smvvalli58@gmail.com>
2025-06-02 16:31:20 +0530

    Add files via upload

---
+++ CompareTwoStringsWithoutEqual.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class CompareTwoStringsWithoutEqual{
+    public static void main (String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String a=sc.nextLine();
+        String b=sc.nextLine();
+        if(a.length()!=b.length())
+        {
+             System.out.println(false);
+              System.exit(0);
+        }
+        for(int i=0;i<a.length();i++)
+        {
+             System.out.println(false);
+              System.exit(0);
+        }
+         System.out.println(true);
+       
+            
+         
+    }   
+
+    
+}
